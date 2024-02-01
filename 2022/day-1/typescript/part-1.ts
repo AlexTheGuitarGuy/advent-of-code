670a97187e609a5a598f9b5eb9ab8e57f26349e7
Alex <seremet.alexandru.work@gmail.com>
2024-02-01 14:40:46 +0200

    solved day 1 part 1

--- 2022/day-1/typescript/part-1.ts
+++ 2022/day-1/typescript/part-1.ts
@@ -2,4 +2,16 @@ import { readFileSync } from 'fs'
 
 const file = readFileSync('2022/day-1/input.txt', 'utf-8')
 
-console.log('file', file)
+const summedCalories = file.split('\n').reduce(
+    (accumulator, current) => {
+        if (current === '') return [...accumulator, 0]
+        return [
+            ...accumulator.slice(0, accumulator.length + 1),
+            accumulator[accumulator.length - 1] + +current,
+        ]
+    },
+    [0]
+)
+
+const result = summedCalories.sort((a, b) => b - a)[0]
+console.log('result: ', result)
